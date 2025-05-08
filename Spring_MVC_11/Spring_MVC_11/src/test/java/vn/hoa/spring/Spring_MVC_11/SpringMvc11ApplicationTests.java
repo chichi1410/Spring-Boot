7a1013e3c19dff2381928bb45b5bada5f8249cb7
chichi1410 <hoatkhtk2004@gmail.com>
2025-05-08 19:41:26 +0700

    update 8/5

---
+++ Spring_MVC_11/Spring_MVC_11/src/test/java/vn/hoa/spring/Spring_MVC_11/SpringMvc11ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hoa.spring.Spring_MVC_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
